a339f16fdbfefc63656c14433369660c6696563e
sebastien.gourmand <sebastien.gourmand@glasshouse.fr>
2015-09-14 15:15:16 +0200

    robot

--- wp-content/plugins/MyPlugin/inc/functions.php
+++ wp-content/plugins/MyPlugin/inc/functions.php
@@ -21,12 +21,13 @@ function simple_format($txt){
     $txt = preg_replace('/-+/', '-', $txt);
     return trim($txt);
 }
+$condition_update = 'VALUES(prix_apres) < prix_apres OR VALUES(link)=link'; // on ecrase si la valeur du prix apres est plus petite, ou si le lien est le meme ( un meme site peu augmenter ses tarif, mais si c'est un site diff, on garde le prix le plus faible )
 
 $sql = 'INSERT INTO game (titre,simple_titre,link,img,prix_avant,prix_apres,percent,creato) VALUES(?,?,?,?,?,?,?,now()) ON DUPLICATE KEY UPDATE creato=now(),'
-                    . ' percent = IF(VALUES(prix_apres) < prix_apres, VALUES(percent),percent),'
-                    . ' prix_avant = IF(VALUES(prix_apres) < prix_apres, VALUES(prix_avant),prix_avant),'
-                    . ' link = IF(VALUES(prix_apres) < prix_apres, VALUES(link),link),'
-                    . ' prix_apres = IF(VALUES(prix_apres) < prix_apres, VALUES(prix_apres),prix_apres)'
+                    . ' percent = IF('.$condition_update.', VALUES(percent),percent),'
+                    . ' prix_avant = IF('.$condition_update.', VALUES(prix_avant),prix_avant),'
+                    . ' link = IF('.$condition_update.', VALUES(link),link),'
+                    . ' prix_apres = IF('.$condition_update.', VALUES(prix_apres),prix_apres)'
                     ;
 ?>
 
